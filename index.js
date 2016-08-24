baca5c732c61c58fb013717d7276c7c6adbc085f
Nathan Dao <nathan@guynathan.com>
2016-08-24 21:12:27 +0300

    update port

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let server = require('./server');
-
-let port = process.env.NODE_ENV === 'production' ? 80 : 3000;
+let port = process.env.PORT || 3000;
 
 server.listen(port, function() {
   console.log('listening to http://localhost:' + port)
